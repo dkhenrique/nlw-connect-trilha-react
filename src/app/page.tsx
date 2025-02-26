dccbe12be5144eb00a1d7d6a522643d1eeec41d2
Daniel Henrique <daniel.dk47@gmail.com>
2025-02-25 21:30:49 -0300

    nlw trilha react

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,24 @@
+import Image from "next/image";
+import logo from "../assets/logo.svg";
+
 export default function Home() {
   return (
-    <main>
-      <div>Hello world!</div>
+    <main className="max-w-[1240] mx-auto px-5 py-8 md:py-0">
+      <div className="min-h-dvh flex flex-col justify-center gap-16">
+        <div className="flex flex-col gap-8 items-center md:items-start ">
+          <Image
+            src={logo}
+            alt="DevStage"
+            width={108.5}
+            height={30}
+          />
+
+          <h1 className="text-4xl font-heading text-center leading-none font-medium flex flex-col md:text-7xl md:text-left">
+            <span className="text-blue">CodeCraft</span>
+            Summit 2025
+          </h1>
+        </div>
+      </div>
     </main>
   );
 }
